7f173944f33ac3af78116cde7e708c456851d360
rsgilbert <gssenyonjo@gmail.com>
2019-08-26 17:10:34 +0300

    added allowed hosts

--- shopping/settings.py
+++ shopping/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,10 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'i(w8&5cca-2b%$)=1q*3lja!_0#d)j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '<domain name>',
+    '<Ip address>',
+]
 
 
 # Application definition
@@ -133,7 +137,6 @@ AUTH_USER_MODEL = 'account.User'
 
 
 # Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
