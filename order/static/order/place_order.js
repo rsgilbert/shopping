d3fd56c2d608fd4231b7b36484662dacc96e71ea
rsgilbert <gssenyonjo@gmail.com>
2019-08-29 02:01:18 +0300

    done for tonight. Everything working almost. Remaining with configuring modal

--- order/static/order/place_order.js
+++ order/static/order/place_order.js
@@ -1,7 +1,7 @@
 let BACKDROP = document.querySelector('.backdrop')
 let MODAL = document.querySelector('.modal')
 let PLACEORDER = document.querySelector('.submit_placeorder')
-
+let FORM = document.qu
 
 PLACEORDER.addEventListener('click', () => {
     BACKDROP.style.display = 'block'
