5276a2e3d891bc577cc4aac50b7ceb6377d4f42c
rsgilbert <gssenyonjo@gmail.com>
2019-08-28 22:34:27 +0300

    about to finish designing form

--- order/models.py
+++ order/models.py
@@ -10,3 +10,11 @@ class Order(models.Model):
     commodity = models.ForeignKey(to=Commodity, on_delete=models.CASCADE)
     hall = models.CharField(max_length=100)
     room_number = models.CharField(max_length=10)
+
+
+class OrderHistory(models.Model):
+    fullname = models.CharField(max_length=100)
+    telephone = models.CharField(max_length=100)
+    commodity = models.ForeignKey(to=Commodity, on_delete=models.CASCADE)
+    hall = models.CharField(max_length=100)
+    room_number = models.CharField(max_length=10)
--- order/views.py
+++ order/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .forms import OrderForm
 from commodity.models import Commodity
-from .models import Order
+from .models import Order, OrderHistory
 
 # Create your views here.
 
@@ -9,12 +9,19 @@ def place_order(request, id):
     commodity = get_object_or_404(Commodity, pk=id)
     if request.method == "GET":
         form = OrderForm()
-        return render(request, 'order/place_order.html', {'form': form, "id": id, 'commodity': commodity})
+        return render(request, 'order/place_order.html', {'form': form.as_table(), "id": id, 'commodity': commodity})
 
     if request.method == "POST":
         form = OrderForm(request.POST)
         if form.is_valid():
             Order.objects.create(
+                fullname=form.cleaned_data['fullname'],
+                telephone=form.cleaned_data['telephone'],
+                hall=form.cleaned_data['hall'],
+                room_number=form.cleaned_data['room_number'],
+                commodity=commodity
+                )
+            OrderHistory.objects.create(
                 fullname=form.cleaned_data['fullname'],
                 telephone=form.cleaned_data['telephone'],
                 hall=form.cleaned_data['hall'],
