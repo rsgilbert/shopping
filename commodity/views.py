987654d08bc4bff8306fb835bdfd076524da3caf
rsgilbert <gssenyonjo@gmail.com>
2019-09-01 17:21:19 +0300

    completed search box

--- commodity/views.py
+++ commodity/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 from .models import Commodity
+from .forms import SearchForm
 
 # Create your views here.
 
 
 def commodities(request):
-    if request.method == 'GET':
-        items = Commodity.objects.all()
-        return render(request, 'commodity/commodities.html', {'items': items})
+	if request.method == 'GET':
+		items = Commodity.objects.all()
+		form = SearchForm()
+		return render(request, 'commodity/commodities.html', {'items': items, 'form': form})
 
-    if request.method == "POST":
-            print("Method was post", request.POST)
-            searchword = request.POST['searchitem']
-            print("Search word is ", searchword)    
-            items = Commodity.objects.filter(name__icontains=searchword)
-            return render(request, 'commodity/commodities.html', {'items': items})
+	if request.method == "POST":
+		form = SearchForm(request.POST)
+		if form.is_valid():
+			
+			search_text = form.cleaned_data['text']
+			print("Search word is ", search_text)    
+			items = Commodity.objects.filter(name__icontains=search_text)
+			return render(request, 'commodity/commodities.html', {'items': items, 'form': SearchForm()})
 
-
+		else:
+			print(form.errors)
+			return HttpResponse(form.errors)
